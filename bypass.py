84c8a8e6ded9ea354cd6251010031647a7802322
Tomatrash <91646905+Tomatrash@users.noreply.github.com>
2022-01-19 03:06:35 -0300

    Update bypass.py

--- bypass.py
+++ bypass.py
@@ -1,6 +1,1 @@
-#Importar
- tomada de importação
-# cria um socket INET, STREAMing
-s   =   soquete . socket ( socket . AF_INET , socket . SOCK_STREAM )
 
-# agora conecte-se ao servidor web na porta 80 - o normalEm
